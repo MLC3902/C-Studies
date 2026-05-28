df7308b58d34f657d7ddba548ab50d1bfee85ae9
MariaLuiza <luizacardoso0303@gmail.com>
2026-05-28 19:57:08 -0300

    Create elementos_de_matriz_printando.c

---
+++ elementos_de_matriz_printando.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int matriz[3][3];
+	
+	matriz[0][0] = 1;
+	matriz [0][1] = 2;
+	matriz [0][2] = 3;
+	matriz [1][0] = 4;
+	matriz [1][1] = 5;
+	matriz [1][2] = 6;
+	matriz [2][0] = 7;
+	matriz [2][1] = 8;
+	matriz [2][2] = 9;
+	
+	 printf("%d\n", matriz [0][2]);
+	return 0;
+}
